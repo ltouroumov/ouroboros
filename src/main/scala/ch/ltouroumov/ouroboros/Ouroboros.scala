0ef72a32c95c3ef19851c8c4446bb9ea49976e1d
Laureline Valérie David <ltouroumov@gmail.com>
2021-10-20 22:19:21 +0200

    Stuff

---
+++ src/main/scala/ch/ltouroumov/ouroboros/Ouroboros.scala
@@ -0,0 +1,105 @@
+package ch.ltouroumov.ouroboros
+
+import net.minecraft.block.AbstractBlock.Properties
+import net.minecraft.block.material.Material
+import net.minecraft.block.{Block, Blocks}
+import net.minecraft.item.{BlockItem, Item}
+import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.event.RegistryEvent
+import net.minecraftforge.eventbus.api.SubscribeEvent
+import net.minecraftforge.fml.InterModComms
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.event.lifecycle.{FMLClientSetupEvent, FMLCommonSetupEvent, InterModEnqueueEvent, InterModProcessEvent}
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
+import org.apache.logging.log4j.LogManager
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Ouroboros.MOD_ID)
+object Ouroboros {
+  final val MOD_ID = "ouroboros"
+  // Directly reference a log4j logger.
+  private val LOGGER = LogManager.getLogger
+
+  // Register the setup method for modloading
+  FMLJavaModLoadingContext.get.getModEventBus.addListener(this.setup)
+  // Register the enqueueIMC method for modloading
+  FMLJavaModLoadingContext.get.getModEventBus.addListener(this.enqueueIMC)
+  // Register the processIMC method for modloading
+  FMLJavaModLoadingContext.get.getModEventBus.addListener(this.processIMC)
+  // Register the doClientStuff method for modloading
+  FMLJavaModLoadingContext.get.getModEventBus.addListener(this.doClientStuff)
+
+  Registry.BLOCKS.register(MinecraftForge.EVENT_BUS)
+  Registry.ITEMS.register(MinecraftForge.EVENT_BUS)
+
+  // Register ourselves for server and other game events we are interested in
+  MinecraftForge.EVENT_BUS.register(this)
+
+  private def setup(event: FMLCommonSetupEvent): Unit = {
+    // some preinit code
+    LOGGER.info("HELLO FROM PREINIT")
+    LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName)
+  }
+
+  private def doClientStuff(event: FMLClientSetupEvent): Unit = {
+    // do something that can only be done on the client
+    LOGGER.info("Got game settings {}", event.getMinecraftSupplier.get.options)
+  }
+
+  private def enqueueIMC(event: InterModEnqueueEvent): Unit = {
+    // some example code to dispatch IMC to another mod
+    InterModComms.sendTo("ouroboros", "helloworld", () => {
+      LOGGER.info("Hello world from the MDK")
+      "Hello world"
+    })
+  }
+
+  private def processIMC(event: InterModProcessEvent): Unit = {
+    // some example code to receive and process InterModComms from other mods
+    import scala.jdk.StreamConverters._
+    val messages = event.getIMCStream.toScala(LazyList).map(_.getMessageSupplier[String].get())
+    LOGGER.info("Got IMC {}", messages.mkString(", "))
+  }
+
+  // You can use SubscribeEvent and let the Event Bus discover methods to call
+  @SubscribeEvent
+  def onServerStarting(event: FMLServerStartingEvent): Unit = {
+    // do something when the server starts
+    LOGGER.info("HELLO from server starting")
+  }
+
+}
+
+object Registry {
+
+  import net.minecraftforge.registries.DeferredRegister
+  import net.minecraftforge.registries.ForgeRegistries
+
+  val BLOCKS: DeferredRegister[Block] = DeferredRegister.create(ForgeRegistries.BLOCKS, Ouroboros.MOD_ID)
+  val ITEMS: DeferredRegister[Item] = DeferredRegister.create(ForgeRegistries.ITEMS, Ouroboros.MOD_ID)
+
+
+
+}
+
+// You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
+// Event bus for receiving Registry Events)
+// The object must be at the top-level. Don't forget to fill modid.
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = Ouroboros.MOD_ID)
+object RegistryEvents {
+  private val LOGGER = LogManager.getLogger
+
+
+  @SubscribeEvent
+  def onBlocksRegistry(blockRegistryEvent: RegistryEvent.Register[Block]): Unit = {
+    // register a new block here
+    LOGGER.info("HELLO from Register Block")
+  }
+
+  @SubscribeEvent
+  def onItemsRegistry(blockRegistryEvent: RegistryEvent.Register[Item]): Unit = {
+    // register a new block here
+    LOGGER.info("HELLO from Register Item")
+  }
+}
